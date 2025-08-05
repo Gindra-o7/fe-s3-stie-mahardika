79ebb82ead0ac832f4988040c7d326914b67a754
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-08-05 20:45:11 +0700

    Remove unused router imports for koordinatorKPRouter and instansiRouter to streamline routing configuration.

--- src/routers/app.router.tsx
+++ src/routers/app.router.tsx
@@ -2,15 +2,11 @@ import { createBrowserRouter } from "react-router-dom";
 import { generalRouter } from "./general.router";
 import { mahasiswaRouter } from "./mahasiswa.router";
 import { dosenRouter } from "./dosen.router";
-import { koordinatorKPRouter } from "./koordinator-kp.router";
-import { instansiRouter } from "./pembimbing-instansi.router";
 
 const router = createBrowserRouter([
 	...generalRouter,
 	...mahasiswaRouter,
 	...dosenRouter,
-	...koordinatorKPRouter,
-	...instansiRouter	
 ]);
 
 export default router;
