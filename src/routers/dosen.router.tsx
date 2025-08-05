5f76847bf73a6ea5bd13f7478dbbd15731090670
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-08-05 20:42:41 +0700

    Remove unused API services and router for Pembimbing Instansi and Mahasiswa, cleaning up the codebase by deleting obsolete files related to bimbingan, seminar, daily reports, and daftar KP functionalities.

--- src/routers/dosen.router.tsx
+++ src/routers/dosen.router.tsx
@@ -1,10 +1,6 @@
 import ProtectedRoute from "./protected.router";
 import DosenSetoranHafalanMahasiswaPAPage from "@/pages/dosen/setoran-hafalan/mahasiswa-pa/page";
 import DetailMahasiswaSetoran from "@/pages/dosen/setoran-hafalan/mahasiswa-pa/DetailMahasiswaSetoran";
-import DosenPengujiNilaiPage from "@/pages/dosen/seminar-kerja-praktik/nilai-penguji/page";
-import NilaiSeminarPenguji from "@/pages/dosen/seminar-kerja-praktik/nilai-penguji/NilaiSeminarPenguji";
-import DosenKerjaPraktikMahasiswaBimbingPage from "@/pages/dosen/kerja-praktik/mahasiswa-bimbing/page";
-import DosenKerjaPraktikMahasiswaBimbingDetailPage from "@/pages/dosen/kerja-praktik/mahasiswa-bimbing/detail/page";
 
 export const dosenRouter = [
   {
@@ -22,37 +18,5 @@ export const dosenRouter = [
         <DetailMahasiswaSetoran />
       </ProtectedRoute>
     ),
-  },
-  {
-    path: "/dosen/kerja-praktik/mahasiswa-bimbing",
-    element: (
-      <ProtectedRoute roles={["dosen"]}>
-        <DosenKerjaPraktikMahasiswaBimbingPage />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: "/dosen/kerja-praktik/mahasiswa-bimbing/detail",
-    element: (
-      <ProtectedRoute roles={["dosen"]}>
-        <DosenKerjaPraktikMahasiswaBimbingDetailPage />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: "/dosen/seminar-kp/nilai-penguji",
-    element: (
-      <ProtectedRoute roles={["dosen"]}>
-        <DosenPengujiNilaiPage />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: "/dosen/seminar-kp/nilai-penguji/input-nilai",
-    element: (
-      <ProtectedRoute roles={["dosen"]}>
-        <NilaiSeminarPenguji />
-      </ProtectedRoute>
-    ),
-  },
+  }
 ];
