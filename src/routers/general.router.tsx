a4562d35ca8bc799a5cb16c3013a71b243a95850
Gindra-o7 <gilangindra734@gmail.com>
2025-09-30 19:30:48 +0700

    manage route

--- src/routers/general.router.tsx
+++ src/routers/general.router.tsx
@@ -3,7 +3,6 @@ import ForbiddenPage from "@/pages/publics/forbidden.page";
 import LandingPage from "@/pages/publics/landing.page";
 import NotFoundPage from "@/pages/publics/not-found.page";
 import KartuRekapanMurojaahPAPage from "@/pages/publics/kartu-rekapan-muroja'ah-pa";
-import DashboardPendaftar from "@/pages/pendaftar/page";
 
 export const generalRouter = [
 	{
@@ -29,9 +28,5 @@ export const generalRouter = [
 	{
 		path: "/kartu-rekapan-murojaah-pa/:id",
 		element: <KartuRekapanMurojaahPAPage/>,
-	},
-	{
-		path: "/pendaftar",
-		element: <DashboardPendaftar/>,
 	},
 ]
